5022a6c0bf9cbd540327312788227227c19bf396
Paul O'Shea <poshea48@msn.com>
2019-12-26 19:55:35 -1000

    added viewport meta tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,6 +33,10 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
         {
+          name: "viewport",
+          content: "initial-scale=1, viewport-fit=cover",
+        },
+        {
           property: `og:title`,
           content: title,
         },
