562f324c91dd5342921a524e5184be8592edd4ec
Paul O'Shea <poshea48@msn.com>
2020-01-31 16:40:47 -1000

    Added cms functionality for blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-styled-components",
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
