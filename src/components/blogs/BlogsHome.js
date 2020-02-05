aacce3f71c5db391af0db6f1aefe7b5edae45431
Paul O'Shea <poshea48@msn.com>
2020-02-05 12:06:34 -1000

    removed height and width in BlogsHome container

--- src/components/blogs/BlogsHome.js
+++ src/components/blogs/BlogsHome.js
@@ -8,10 +8,10 @@ const Container = styled.div`
   background: #f0f8ff;
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  /* width: 100vw;
+  height: 100vh; */
   padding: 2em;
-  overflow: scroll;
+  /* overflow: scroll; */
   h1 {
     align-self: center;
     margin: 0;
